afb4cab55691eeabd5d478aa69fb7ab41d975c47
OldSem <old_sem@meta.ua>
2023-12-28 11:36:16 +0200

    try override

--- scripts/alt_hold.py
+++ scripts/alt_hold.py
@@ -8,7 +8,40 @@ vehicle = connect('udp:127.0.0.1:14550', baud=57600)
 
 
 # Get all original channel values (before override)
-while True:
-    vehicle.channels.overrides['2'] = 200
-    print("Channel values from RC Tx:", vehicle.channels, vehicle.mode, vehicle.location.global_relative_frame)
-    time.sleep(1)
+# Override channels
+print("\nChannel overrides: %s" % vehicle.channels.overrides)
+
+print("Set Ch2 override to 200 (indexing syntax)")
+vehicle.channels.overrides['2'] = 200
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+print(" Ch2 override: %s" % vehicle.channels.overrides['2'])
+
+print("Set Ch3 override to 300 (dictionary syntax)")
+vehicle.channels.overrides = {'3':300}
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+print("Set Ch1-Ch8 overrides to 110-810 respectively")
+vehicle.channels.overrides = {'1': 110, '2': 210,'3': 310,'4':4100, '5':510,'6':610,'7':710,'8':810}
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+
+# Clear override by setting channels to None
+print("\nCancel Ch2 override (indexing syntax)")
+vehicle.channels.overrides['2'] = None
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+print("Clear Ch3 override (del syntax)")
+del vehicle.channels.overrides['3']
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+print("Clear Ch5, Ch6 override and set channel 3 to 500 (dictionary syntax)")
+vehicle.channels.overrides = {'5':None, '6':None,'3':500}
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+print("Clear all overrides")
+vehicle.channels.overrides = {}
+print(" Channel overrides: %s" % vehicle.channels.overrides)
+
+#Close vehicle object before exiting script
+print("\nClose vehicle object")
+vehicle.close()
