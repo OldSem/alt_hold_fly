7f6cf10ff92dc8712973d118eb04cc5216ea3c77
OldSem <old_sem@meta.ua>
2023-12-28 09:42:05 +0200

    remove wait redy

--- scripts/alt_hold.py
+++ scripts/alt_hold.py
@@ -4,7 +4,7 @@ from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelativ
 connection_string = 'udp:127.0.0.1:14550'
 
 print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect('udp:127.0.0.1:14550', baud=57600, wait_ready=True)
+vehicle = connect('udp:127.0.0.1:14550', baud=57600)
 
 # Get all original channel values (before override)
 print("Channel values from RC Tx:", vehicle.channels)
