0f575a41094a6c36fbb0f32482c67869c2ec76a8
OldSem <old_sem@meta.ua>
2023-12-28 09:45:50 +0200

    show mode

--- scripts/alt_hold.py
+++ scripts/alt_hold.py
@@ -1,5 +1,5 @@
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
-
+import time
 
 connection_string = 'udp:127.0.0.1:14550'
 
@@ -7,4 +7,6 @@ print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect('udp:127.0.0.1:14550', baud=57600)
 
 # Get all original channel values (before override)
-print("Channel values from RC Tx:", vehicle.channels)
+while True:
+    print("Channel values from RC Tx:", vehicle.channels, vehicle.mode, vehicle.location.global_relative_frame)
+    time.sleep(1)
