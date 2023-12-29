ef4236ce519cf6f18e6b787c838b72bf5ddadc9a
OldSem <old_sem@meta.ua>
2023-12-29 11:17:50 +0200

    create drone class

--- scripts/alt_hold.py
+++ scripts/alt_hold.py
@@ -1,47 +1,26 @@
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 import time
+import geopy.distance
 
 connection_string = 'udp:127.0.0.1:14550'
 
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect('udp:127.0.0.1:14550', baud=57600)
+start_position = vehicle.location.global_relative_frame
+ROLL = 1500
+PITCH = 1500
+THROTTLE = 1500
+YAW = 1500
+
+def dummy_goto(wpl):
+
+    left = geopy.distance.geodesic([wpl.lat, wpl.lon], [start_position.lat, start_position.lon])
+
+    while True:
+        old_left = left
+        time.sleep(1)
+        position = vehicle.location.global_relative_frame
+        left = geopy.distance.geodesic([wpl.lat, wpl.lon], [position.lat, position.lon])
 
 
-# Get all original channel values (before override)
-# Override channels
-print("\nChannel overrides: %s" % vehicle.channels.overrides)
-
-print("Set Ch2 override to 200 (indexing syntax)")
-vehicle.channels.overrides['2'] = 200
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-print(" Ch2 override: %s" % vehicle.channels.overrides['2'])
-
-print("Set Ch3 override to 300 (dictionary syntax)")
-vehicle.channels.overrides = {'3':300}
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-print("Set Ch1-Ch8 overrides to 110-810 respectively")
-vehicle.channels.overrides = {'1': 110, '2': 210,'3': 310,'4':4100, '5':510,'6':610,'7':710,'8':810}
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-
-# Clear override by setting channels to None
-print("\nCancel Ch2 override (indexing syntax)")
-vehicle.channels.overrides['2'] = None
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-print("Clear Ch3 override (del syntax)")
-del vehicle.channels.overrides['3']
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-print("Clear Ch5, Ch6 override and set channel 3 to 500 (dictionary syntax)")
-vehicle.channels.overrides = {'5':None, '6':None,'3':500}
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-print("Clear all overrides")
-vehicle.channels.overrides = {}
-print(" Channel overrides: %s" % vehicle.channels.overrides)
-
-#Close vehicle object before exiting script
-print("\nClose vehicle object")
 vehicle.close()
---
+++ scripts/drone.py
@@ -0,0 +1,85 @@
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+import time
+import geopy.distance
+
+
+class Action:
+    delta = 10
+
+    def __init__(self, channel, value):
+        self.channel = channel
+        self.value = value
+
+    def correct(self):
+        self.delta = -self.delta
+        self.value += self.delta * 2
+
+    def faster(self):
+        self.value += self.value
+
+    def slower(self):
+        self.value -= self.value
+
+
+class Drone:
+    roll = Action(channel=1, value=1500)
+    pitch = Action(channel=2, value=1500)
+    throttle = Action(channel=3, value=1500)
+    yaw = Action(channel=4, value=1500)
+
+    wpl: LocationGlobalRelative
+    last_distance: float
+    last_delta: float
+
+    def __init__(self, connection_string='udp:127.0.0.1:14550'):
+        try:
+            self.vehicle = connect(connection_string, baud=57600)
+        except:
+            self.vehicle = None
+
+    @property
+    def position(self):
+        return self.vehicle.location.global_relative_frame
+
+    @property
+    def left(self):
+        return geopy.distance.geodesic([self.wpl.lat, self.wpl.lon],
+                                       [self.position.lat, self.position.lon]).m
+
+    @property
+    def delta(self):
+        return self.last_distance - self.left
+
+    @property
+    def channels(self):
+        return {action.channel: action.value for action in [self.roll, self.pitch, self.throttle, self.yaw]}
+
+    def set_direction(self, action):
+        self.last_distance = self.left
+        print(self.left)
+        self.last_delta = self.last_delta
+        action.faster()
+        time.sleep(0.5)
+        if self.left > self.last_distance or self.delta < self.last_delta:
+            action.correct()
+        self.last_distance = self.left
+        self.last_delta = self.last_delta
+
+    def dummy_goto(self, wpl):
+
+        self.turn_off(10)
+        self.wpl = LocationGlobalRelative(*wpl)
+        while self.left < 0.2:
+            self.set_direction(self.roll)
+            self.set_direction(self.pitch)
+
+    def turn_off(self, alt):
+        while True:
+            self.throttle.value = 1900
+            self.vehicle.channels.overrides = self.channels
+            time.sleep(1)
+            if self.position.alt >= alt:
+                self.throttle.value = 1500
+                self.vehicle.channels.overrides = self.channels
+                break
+
